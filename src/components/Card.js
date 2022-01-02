f0302b5ed6d3e11f0a8051fa7f47ad5e3b888427
An-Nguyen-02 <nhatan020902@gmail.com>
2022-01-02 10:30:35 +0200

    improve performance

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({name,email,id}) => {
 	return (
 		<div className="bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
-			<img alt ='robot_img' src={`https://robohash.org/${id}?200x200`} />
+			<img alt ='robot_img' src={`https://robohash.org/${id}?200x200`} width='200px' height='auto' />
 			<div>
 				<h2> {name} </h2>
 				<p> {email} </p>

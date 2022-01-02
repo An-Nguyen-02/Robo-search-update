fe5b0cc933ed3926691e03da09682cf99394bfb7
An-Nguyen-02 <nhatan020902@gmail.com>
2022-01-02 10:38:11 +0200

    add desc, img size

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,6 +4,7 @@ const Searchbox = ({ searchChange}) => {
 	return(
 		<div className="pa2 tc">
 			<input 
+				aria-label= 'Search Robots'
 				placeholder="Find robots..."
 				type="search"
 				className='pa3 ba b--green bg-lightest-blue'
